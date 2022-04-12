64fa7e3c311840ba4017a4e865c3ede1300ecc49
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-12 17:41:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+met = int(input("Qual o valor em metros: "))
+
+cent = met*100
+mili = met*1000
+
+print("Valor convertido para centimetros: ",cent)
+print("Valor convertido para milimetros: ",mili)
